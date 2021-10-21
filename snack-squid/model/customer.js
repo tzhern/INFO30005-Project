008fb8577ade131e0c84b5a42f29d1a29e809240
Zhi Hern Tom <23621760+tzhern@users.noreply.github.com>
2021-10-21 23:36:26 +0800

    Update bcrypt model

--- snack-squid/model/customer.js
+++ snack-squid/model/customer.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const bcrypt = require('bcrypt-nodejs')
+const bcrypt = require('bcryptjs')
 const customerSchema = new mongoose.Schema({
     givenName: { type: String, required: true },
     familyName: { type: String, required: true },
